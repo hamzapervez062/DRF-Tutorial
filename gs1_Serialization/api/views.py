a23c98c6a9cc731cd589dd807443cf25b1dcc867
19014156-062 <19014156-062@uog.edu.pk>
2024-08-10 23:42:59 +0500

    'CRUD'

--- gs1_Serialization/api/views.py
+++ gs1_Serialization/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.renderers import JSONRenderer
 # Model object- Single Student Data
 def student_detail(request):
     stu = Student.objects.get(pk = 1)
-    serializer = StudentSerializer(stu,many=False)
+    serializer = StudentSerializer(stu, many=False)
     print(serializer.data)
 
     json_data = JSONRenderer().render(serializer.data)
