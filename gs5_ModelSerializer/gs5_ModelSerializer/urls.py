803148afcd75085040b215e796eef9fee3bd6622
19014156-062 <19014156-062@uog.edu.pk>
2024-08-12 21:58:42 +0500

    ModelSerializer

---
+++ gs5_ModelSerializer/gs5_ModelSerializer/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('student_api/', views.student_api),
+    #CBV
+    path('student_apicbv/', views.STUDENTAPIVIEW.as_view()),
+]
+
